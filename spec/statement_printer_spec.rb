9a71e1a5f4caccaa4047878e95f36730d7128174
Leigh-ann <leighann.gant@gmail.com>
2018-02-26 17:56:58 +0000

    Readme updated

--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -3,7 +3,6 @@ require 'statement_printer'
 describe StatementPrinter do
   subject(:transaction2) { double :transaction, credit?: false, debit?: true, balance: 125, date_created: '26/02/2018', amount: 25 }
   subject(:transaction1) { double :transaction, credit?: true, balance: 150, date_created: '26/02/2018', amount: 50 }
-
   subject(:statement_printer) { described_class.new }
 
   describe '#print((transactions)' do
